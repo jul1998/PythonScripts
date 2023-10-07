2edae0ff6af67f881ca8ed110620dac71bac9607
Julian <jguevara321@gmail.com>
2023-10-07 15:23:25 -0600

    Enhanced script

--- main.py
+++ main.py
@@ -2,12 +2,32 @@ import pandas as pd
 import json
 
 
+def replace_single_quotes_with_double(json_str):
+    """
+    Replace single quotes with double quotes in a JSON string.
+    :param json_str: JSON string
+    :return: str JSON string
+    """
+    if isinstance(json_str, str): # Check if the input is a string
+        return json_str.replace("'", "\"") # Replace single quotes with double quotes
+    return json_str
+
+def replace_quotes_in_columns(df, columns_to_replace):
+    """
+    Replace single quotes with double quotes in specified columns of a DataFrame.
+    :param df: DataFrame
+    :param columns_to_replace: List of column names to process
+    :return: DataFrame
+    """
+    for col in columns_to_replace: # Iterate over columns
+        df[col] = df[col].apply(replace_single_quotes_with_double) # Apply the function to each column
+    return df
 
 def extract_id_value_pairs(json_str):
     """
     Extract id-value pairs from json string
-    :param json_str:
-    :return:  dict
+    :param json_str: JSON string
+    :return:  dict of id-value pairs
     """
     try:
         data = json.loads(json_str)
@@ -22,27 +42,32 @@ def extract_id_value_pairs(json_str):
 def remove_useless_columns(df, *cols_to_remove):
     """
     Remove useless columns
-    :param df:
-    :param cols_to_remove:
+    :param df: DataFrame
+    :param cols_to_remove: List of column names to remove
     :return:  DataFrame
     """
     df_copy = df.copy()
     df_copy.drop(columns=list(cols_to_remove), inplace=True)
     return df_copy
 
-def insert_json_cols(df, target_columns:str):
+
+def insert_json_cols(df, target_columns):
     """
-    Insert json columns
-    :param df:
-    :param target_columns:
-    :return:
+    Insert JSON columns for multiple target columns.
+    :param df: DataFrame
+    :param target_columns: List of target column names
+    :return: DataFrame
     """
     df_copy = df.copy()
-    df_copy['customfields_dict'] = df_copy[target_columns].apply(extract_id_value_pairs)
-    customfields_df = pd.DataFrame(df_copy['customfields_dict'].tolist(), index=df_copy.index)
 
-    result_df = pd.concat([df_copy, customfields_df], axis=1)
-    return result_df
+    for col in target_columns:
+        df_copy[f'{col}_dict'] = df_copy[col].apply(extract_id_value_pairs) # Extract id-value pairs
+        customfields_df = pd.DataFrame(df_copy[f'{col}_dict'].tolist(), index=df_copy.index) # Convert dict to DataFrame
+        df_copy = pd.concat([df_copy, customfields_df], axis=1) # Concatenate the two DataFrames
+        df_copy = remove_useless_columns(df_copy, f'{col}_dict') # Remove the useless column, Comment this line if you want to keep the column
+
+
+    return df_copy
 
 
 def concat_df(*df):
@@ -53,16 +78,33 @@ def concat_df(*df):
     """
     return pd.concat(df, axis=1)
 
-def get_final_result(target_df):
+def get_final_result(target_df, target_columns , final_file_name='final_df.xlsx' ):
+    """
+    Get final result
+    :param target_df:
+    :param remove_cols:
+    :param final_file_name:
+    :return:
+    """
 
-    final_df = insert_json_cols(target_df, 'customfields_number_1')
+    final_df = insert_json_cols(target_df, target_columns) # Insert json columns
 
-    # Remove json columns
-    final_df = remove_useless_columns(final_df, 'customfields_number_1', 'customfields_dict')
+    final_df.to_excel(final_file_name, index=False)
 
-    final_df.to_excel('final_df.xlsx', index=False)
+
 
 if __name__ == '__main__':
-    df = pd.read_excel('base_final_json.xlsx')
-    get_final_result(df)
+    final_file_name = 'df_base_final.xlsx'
+    target_file = 'base_final_json.xlsx'
+
+    target_columns = ['customfields_number'] # Specify target columns
+
+    df = pd.read_excel(target_file)  # Read the excel file
+    df = replace_quotes_in_columns(df, target_columns) # Replace single quotes with double quotes
+
+    get_final_result(df, target_columns=target_columns, final_file_name=final_file_name) # Get final result
+
     print('Done')
+
+
+
