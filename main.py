5a3bf444cc7606f4b69c18d16f46ad16128c5f35
Julian <jguevara321@gmail.com>
2023-10-10 13:03:04 -0600

    Enhanced script

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import pandas as pd
 import json
+import boto3
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv() # Load environment variables
+
+aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID") # Get environment variables
+aws_secret_access_key= os.getenv("AWS_SECRET_ACCESS_KEY") # Get environment variables
+
+
+
 
 
 def replace_single_quotes_with_double(json_str):
@@ -8,9 +20,10 @@ def replace_single_quotes_with_double(json_str):
     :param json_str: JSON string
     :return: str JSON string
     """
-    if isinstance(json_str, str): # Check if the input is a string
-        return json_str.replace("'", "\"") # Replace single quotes with double quotes
+    if isinstance(json_str, str):  # Check if the input is a string
+        return json_str.replace("'", "\"")  # Replace single quotes with double quotes
     return json_str
+
 
 def replace_quotes_in_columns(df, columns_to_replace):
     """
@@ -19,9 +32,10 @@ def replace_quotes_in_columns(df, columns_to_replace):
     :param columns_to_replace: List of column names to process
     :return: DataFrame
     """
-    for col in columns_to_replace: # Iterate over columns
-        df[col] = df[col].apply(replace_single_quotes_with_double) # Apply the function to each column
+    for col in columns_to_replace:  # Iterate over columns
+        df[col] = df[col].apply(replace_single_quotes_with_double)  # Apply the function to each column
     return df
+
 
 def extract_id_value_pairs(json_str):
     """
@@ -38,6 +52,7 @@ def extract_id_value_pairs(json_str):
 
     except (json.JSONDecodeError, TypeError):
         return {}
+
 
 def remove_useless_columns(df, *cols_to_remove):
     """
@@ -61,11 +76,12 @@ def insert_json_cols(df, target_columns):
     df_copy = df.copy()
 
     for col in target_columns:
-        df_copy[f'{col}_dict'] = df_copy[col].apply(extract_id_value_pairs) # Extract id-value pairs
-        customfields_df = pd.DataFrame(df_copy[f'{col}_dict'].tolist(), index=df_copy.index) # Convert dict to DataFrame
-        df_copy = pd.concat([df_copy, customfields_df], axis=1) # Concatenate the two DataFrames
-        df_copy = remove_useless_columns(df_copy, f'{col}_dict') # Remove the useless column, Comment this line if you want to keep the column
-
+        df_copy[f'{col}_dict'] = df_copy[col].apply(extract_id_value_pairs)  # Extract id-value pairs
+        customfields_df = pd.DataFrame(df_copy[f'{col}_dict'].tolist(),
+                                       index=df_copy.index)  # Convert dict to DataFrame
+        df_copy = pd.concat([df_copy, customfields_df], axis=1)  # Concatenate the two DataFrames
+        df_copy = remove_useless_columns(df_copy,
+                                         f'{col}_dict')  # Remove the useless column, Comment this line if you want to keep the column
 
     return df_copy
 
@@ -73,38 +89,52 @@ def insert_json_cols(df, target_columns):
 def concat_df(*df):
     """
     Concatenate dataframes
-    :param df:
+    :param df: List of DataFrames
     :return: DataFrame
     """
     return pd.concat(df, axis=1)
 
-def get_final_result(target_df, target_columns , final_file_name='final_df.xlsx' ):
+
+def get_final_result(target_df, target_columns, final_file_name='final_df.xlsx'):
     """
     Get final result
-    :param target_df:
-    :param remove_cols:
-    :param final_file_name:
-    :return:
+    :param target_df: DataFrame
+    :param remove_cols:     List of columns to remove
+    :param final_file_name: Final file name
+    :return: DataFrame
     """
 
-    final_df = insert_json_cols(target_df, target_columns) # Insert json columns
+    final_df = insert_json_cols(target_df, target_columns)  # Insert json columns
 
-    final_df.to_excel(final_file_name, index=False)
+    final_df.to_csv(final_file_name, index=False)
 
+
+def upload_to_s3(bucket, file_name, key):
+    s3 = boto3.resource(
+        service_name='s3',
+        region_name='us-east-1',
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key
+    )
+
+    s3.Bucket(bucket).upload_file(Filename=file_name, Key=key)
 
 
 if __name__ == '__main__':
-    final_file_name = 'df_base_final.xlsx'
-    target_file = 'base_final_json.xlsx'
+    final_file_name = 'df_other_sim.csv'  # Specify final file name as a csv file to upload to s3
+    target_file = 'json_other_sim.xlsx'
 
-    target_columns = ['customfields_number'] # Specify target columns
+    # S3 required data
+    bucket = 'quicksigth-dashboards'
+    key = 'sim_issues/inventory_removals_test_julsola.csv'
+
+    target_columns = ['customfields_number', 'customfields_date']  # Specify target columns inside the excel file
 
     df = pd.read_excel(target_file)  # Read the excel file
-    df = replace_quotes_in_columns(df, target_columns) # Replace single quotes with double quotes
+    df = replace_quotes_in_columns(df, target_columns)  # Replace single quotes with double quotes
 
-    get_final_result(df, target_columns=target_columns, final_file_name=final_file_name) # Get final result
+    get_final_result(df, target_columns=target_columns, final_file_name=final_file_name)  # Get final result
+
+    upload_to_s3(bucket=bucket, file_name=final_file_name, key=key)  # Upload to s3
 
     print('Done')
-
-
-
